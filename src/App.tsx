d46d4b1d37299f332e3eb80225dc0b421e2864d9
Liza Agey <agey.eliz@gmail.com>
2022-12-11 22:08:52 +0700

    feat: localStorage methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Counter from './components/Counter/Counter';
 
-
-export type MessageType = {
-    id: number,
-    text: string
-}
-
 function App() {
+
+
     return (
         <div className={'App'}>
             <Counter/>
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import Button from '../Button/Button';
-import styles from "./Counter.module.css"
+import styles from './Counter.module.css'
 import './../../App.css';
+import {stringify} from 'querystring';
 
 const Counter: React.FC<any> = () => {
     const MAX_NUMBER = 5
     const MIN_NUMBER = 0
+    let lsNumberKey = 'counterNumberValue'
     const [number, setNumber] = useState<number>(0)
 
     const incNumber = () => {
@@ -16,20 +18,53 @@ const Counter: React.FC<any> = () => {
     const resetNumber = () => {
         setNumber(MIN_NUMBER)
     };
+
+    const setToLSHandler = () => {
+        localStorage.setItem(lsNumberKey, JSON.stringify(number))
+    };
+
+    const getFromLSHandler = () => {
+        let numberAsStr = localStorage.getItem(lsNumberKey)
+        if (numberAsStr) {
+            let newNumber = JSON.parse(numberAsStr)
+            setNumber(newNumber)
+        }
+    };
+
+    const clearLSHandler = () => {
+     localStorage.clear()
+        setNumber(0)
+    };
+
+    const removeItemFromLSHandler = () => {
+     localStorage.removeItem(lsNumberKey)
+    };
     return (
         <div className={styles.Counter}>
             <h3>Counter from 0 to 5</h3>
-            <div className={`${styles.Number} ${number === MAX_NUMBER ? styles.NumberMax : ""}`}>{number}</div>
+            <div className={`${styles.Number} ${number === MAX_NUMBER ? styles.NumberMax : ''}`}>{number}</div>
             <div className={styles.ControlSection}>
                 <Button
-                    name={"INC"}
+                    name={'INC'}
                     onClickCallback={incNumber}
                     disabled={number === MAX_NUMBER}
                 />
                 <Button
-                    name={"RESET"}
+                    name={'RESET'}
                     onClickCallback={resetNumber}
                     disabled={number === MIN_NUMBER}
+                />
+                <Button
+                    name={'Set to LS'}
+                    onClickCallback={setToLSHandler}
+                />
+                <Button
+                    name={'Get from LS'}
+                    onClickCallback={getFromLSHandler}
+                />
+                <Button
+                    name={'Clear LS'}
+                    onClickCallback={clearLSHandler}
                 />
             </div>
         </div>
